a0967fdc87156056d38af147b9cd971c2f26abca
Jakob Hagl <jhagl@sba-research.org>
2019-12-02 15:08:59 +0100

    Add luks support

--- chrootinstall.sh
+++ chrootinstall.sh
@@ -6,7 +6,7 @@ loadkeys de-latin1
 
 source config.conf
 
-#starting installation inside chroot env
+# starting installation inside chroot env
 ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
 hwclock --systohc
 echo ${HOST} > /etc/hostname
@@ -16,7 +16,7 @@ cat >> /etc/hosts <<EOF
 127.0.1.1 $HOST.localdomain $HOST
 EOF
 
-#localisation
+# localisation
 cat >> /etc/locale.gen <<EOF
 en_US.UTF-8 UTF-8
 EOF
@@ -39,15 +39,23 @@ useradd -m -G wheel jakobu5
 # pw change for users
 echo ${USER}':'${PWD} | chpasswd
 
-#installing Linux (was not installed)
-pacman -S --noconfirm linux mkinitcpio dhcpcd
+# setting hooks for kernel
+HOOKS="HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard keymap fsck)"
+echo ${HOOKS} >> /etc/mkinitcpio.conf
 
-#installing UEFI bootloader
-pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+# generating new kernel image
+mkinitpcio -p linux
+
+# change bootloader kernel parameter
+UUID=$(lsblk -dno UUID ${ROOTPART})
+KERNEL_PARAM="cryptdevice=UUID="${UUID}":cryptroot root=/dev/mapper/cryptroot"
+
+
+# installing UEFI bootloader
 mkdir /boot/EFI
 mount ${BOOTPART} /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 exit
-#finished installation in chroot env
+# finished installation in chroot env
