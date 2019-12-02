d8ce20a15c316ec91b2a85cf119365576a25e98f
Jakob Hagl <jhagl@sba-research.org>
2019-12-02 15:08:49 +0100

    Add luks support

--- archinstall.sh
+++ archinstall.sh
@@ -12,34 +12,38 @@ source config.conf
 # no disk encryption
 
 ### PARTITIONING ###
-#for MBR/GRUB - legacy bios
+# for MBR/GRUB - legacy bios
 #parted --script ${HDD} \
 #	mklabel msdos \
 #	mkpart primary xfs 1MiB 100% \
 #  set 1 boot on
 
-#for GPT/GRUB - UEFI
+# for GPT/GRUB - UEFI
 parted --script ${HDD} \
 	mklabel gpt \
 	mkpart P1 fat32 1MiB 512MiB  \
 	mkpart primary xfs 512MiB 100%
 
+# luks
+# preparing non-boot partitions
+echo ${LUKS} | cryptsetup luksFormat -q --type luks1 ${ROOTPART}
+echo ${LUKS} | cryptsetup open ${ROOTPART} cryptroot
 
 # formating the partitions
-mkfs.xfs -f ${ROOTPART}
-mkfs.fat -F32 ${BOOTPART}
+mkfs.xfs -f /dev/mapper/cryptroot
+mkfs.fat -F32 -n EFI ${BOOTPART}
 
 # mount the partitions
-mount ${ROOTPART} /mnt
+mount /dev/mapper/cryptroot /mnt
 
 # TODO: automatically set austria server to the default server
 # https://wiki.archlinux.org/index.php/Mirrors#Sorting_mirrors
 
 ### INSTALLING BASE SYSTEM ###
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel linux mkinitcpio dhcpcd grub efibootmgr dosfstools os-prober mtools
 
 #generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U -L /mnt >> /mnt/etc/fstab
 
 cp chrootinstall.sh /mnt
 cp finishinstall.sh /mnt
